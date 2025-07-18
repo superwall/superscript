3c59b1e3d2881ed09a6930a66d1c93ae6d10e8d3
Ian Rumac <ian.rumac@outlook.com>
2025-07-18 14:59:21 +0200

    Update wasm build script

--- build_wasm.sh
+++ build_wasm.sh
@@ -7,7 +7,7 @@ cargo build --lib --release --target wasm32-unknown-unknown
 echo " 25% - WASM build successful ✅"
 echo " 25% - Ensuring WASM wrapper is built"
 cd wasm
-cargo build --lib --release --target wasm32-unknown-unknown
+RUSTFLAGS='-C target-feature=+bulk-memory' cargo build --lib --release --target wasm32-unknown-unknown
 echo "50% - WASM wrapper build successful ✅"
 
 echo "50% - Building JS bundles"
